4b6cb7e1f53a35df223be71f1d49a14370bc421c
leftenantunicorn <erinbradley27@hotmail.com>
2018-03-31 20:14:18 -0400

    Added python prediction

--- MetricModeller/Controllers/HomeController.cs
+++ MetricModeller/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MetricModeller.Models;
+using System.IO;
+using System.Text;
 
 namespace MetricModeller.Controllers
 {
@@ -12,6 +14,8 @@ namespace MetricModeller.Controllers
     {
         public IActionResult Index()
         {
+            var result = ExecutePythonScript("predictor.py","data.csv", "3,3,3,4,0,1,4,1,2,2,2,3,2,2,2,2,3,3,3,0,3,2,293,1600,25229");
+            ViewData["json"] = "{ \"cost\": 419.178, \"months\": 43.09000000000001}";
             return View();
         }
 
@@ -32,6 +36,36 @@ namespace MetricModeller.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string ExecutePythonScript(string fileName, string dataName, params string[] args)
+        {
+            var pythonInstallationLocation = @"C:\Users\Erin\Anaconda3\python.exe";
+
+            string result;
+            string pathPy = Path.Combine(Directory.GetCurrentDirectory(), @"python\", fileName);
+            string pathCsv = Path.Combine(Directory.GetCurrentDirectory(), @"python\", dataName);
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = pythonInstallationLocation;
+            var argumentsBuilder = new StringBuilder().AppendFormat("{0} {1}", pathPy, pathCsv);
+            foreach (string arg in args)
+            {
+                argumentsBuilder.AppendFormat(" {0}", arg);
+            }
+            start.Arguments = argumentsBuilder.ToString();
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ MetricModeller/python/predictor.py
@@ -0,0 +1,45 @@
+﻿#########################################################
+## Make prediction for single record
+
+try:
+    import sys
+    import numpy as np
+    import pandas as pd
+    from decimal import Decimal
+    from sklearn.preprocessing import Imputer
+    from sklearn.multioutput import MultiOutputRegressor
+    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.cross_validation import train_test_split
+    import simplejson as json
+
+    df = pd.read_csv(sys.argv[1], encoding='utf-8-sig')
+
+    x_single = [[Decimal(n) for n in sys.argv[2].split(",")]];
+
+    # Prepare feature and result sets
+    feature_col_names = ['prec', 'flex', 'resl', 'team', 'pmat', 'rel', 'data', 'cplx', 'ruse', 'docu', 'time', 'stor', 'pvol', 'acap', 'pcap', 'pcon', 'apex', 'plex', 'ltex', 'tool', 'site', 'sched', 'kloc', 'effort', 'defects']
+    predicted_class_names = ['months','cost']
+
+    x = df[feature_col_names].values
+    y = df[predicted_class_names].values
+    split_test_size = 0.30
+
+    
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                    train_size=split_test_size,
+                                                    random_state=4)
+
+    # Manipulate bad data
+    fill_0 = Imputer(missing_values=0, strategy="mean", axis=0)
+    x_train = fill_0.fit_transform(x_train)
+
+    # Train model
+    max_depth = 30
+    rf = RandomForestRegressor(max_depth=max_depth,random_state=0)
+    regr_multirf = MultiOutputRegressor(rf)
+    regr_multirf.fit(x_train, y_train)
+
+    print(json.dumps({"months" : regr_multirf.predict(x_single)[0][0], "cost" : regr_multirf.predict(x_single)[0][1]}), end="")
+
+except Exception as e:
+    print ("Unexpected error:", format(e) )
